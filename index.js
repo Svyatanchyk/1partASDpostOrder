843b0628113bcfdfefc19c9b232f4b0c21d8b348
Svyatan <88565116+Svyatanchyk@users.noreply.github.com>
2021-11-12 00:41:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+class Node {
+   constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+   }
+}
+
+class BinarySearchTree {
+   constructor() {
+      this.root = null;
+   }
+
+   add(value) {
+      const newNode = new Node(value);
+      if (!this.root) {
+         this.root = newNode;
+         return;
+      }
+
+      let currentNode = this.root;
+
+      while (currentNode) {
+         if (newNode.value < currentNode.value) {
+
+            if (!currentNode.left) {
+               currentNode.left = newNode;
+               return
+            }
+
+            currentNode = currentNode.left;
+         } else {
+            if (!currentNode.right) {
+               currentNode.right = newNode;
+               return
+            }
+
+            currentNode = currentNode.right;
+         }
+      }
+   }
+
+   preOrder(node, cb) {
+      if (!node) {
+         return;
+      }
+
+      if (cb) {
+         cb(node)
+      }
+
+      this.preOrder(node.left, cb)
+      this.preOrder(node.right, cb)
+   }
+
+   inOrder(node, cb) {
+      if (!node) {
+         return;
+      }
+
+      if (cb) {
+         cb(node)
+      }
+
+      this.inOrder(node.left, cb)
+
+      if (cb) {
+         cb(node)
+      }
+
+      this.inOrder(node.right, cb)
+   }
+
+   postOrder(node, cb) {
+      if (!node) {
+         return;
+      }
+
+      if (cb) {
+         cb(node)
+      }
+
+      this.postOrder(node.left, cb)
+      this.postOrder(node.right, cb)
+
+      if (cb) {
+         cb(node)
+      }
+
+   }
+
+   traverseDFS(cb, method) {
+      if (method = 'preOrder') {
+         return this.preOrder(this.root, cb)
+      }
+
+      if (method = 'inOrder') {
+         return this.inOrder(this.root, cb)
+      }
+
+      return this.postOrder(this.root, cb)
+   }
+}
+
+const myTree = new BinarySearchTree();
+
+for (let i = 1; i <= 7; i++) {
+   const res = prompt('Введите значение', "")
+   myTree.add(res)
+}
+
+myTree.traverseDFS((node) => {
+   console.log(node.value);
+}, 'postOrder');
+console.log(myTree);
